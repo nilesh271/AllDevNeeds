1dd6095a2eb2aa4d165d4cd204676b8c5fba068a
Nilesh Kumrawat <nilesh271@gmail.com>
2026-07-18 19:46:06 +0530

    fix: resolve Baseurl issue

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={(import.meta as any).env.BASE_URL}>
         <ThemeProvider>
           <App />
         </ThemeProvider>
